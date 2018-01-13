5eeecbff82c2d549c6f8bfea35aa6c5688db6f0e
Shivam Negi <karmashagya97@gmail.com>
2018-01-13 12:21:54 +0530

    Add files via upload

---
+++ ReadFileMain.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+public class ReadFileMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        ReadFile dude=new ReadFile();
+        dude.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
